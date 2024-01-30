2bb88b091ebbe996dd542deeb14381ae913905c8
Alina <alinabokii@gmail.com>
2024-01-30 21:40:40 +0300

    5.14. Модуляция

---
+++ js/util.js
@@ -0,0 +1,13 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+function getRandomArrayElement (elements) {
+  return elements[getRandomInteger(0, elements.length - 1)];
+}
+
+export {getRandomInteger};
+export {getRandomArrayElement};
