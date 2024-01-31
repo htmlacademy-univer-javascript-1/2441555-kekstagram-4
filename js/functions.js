3687f1bc408b3e332dce74914667527965b7ce06
Alina <alinabokii@gmail.com>
2024-01-31 18:44:20 +0300

    5.16. Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -28,3 +28,35 @@ function getNumbers(stringIn) {
 }
 
 getNumbers('jhv 45.4');
+
+function isInWorkTime(dayStart, dayEnd, workStart, workTime) {
+  const workStartHM = workStart.split(':');
+  const workStartHour = Number(workStartHM[0]);
+  const workStartMin = Number(workStartHM[1]);
+
+  const dayStartHM = dayStart.split(':');
+  const dayStartHour = Number(dayStartHM[0]);
+  const dayStartMin = Number(dayStartHM[1]);
+
+  if (workStartHour >= dayStartHour && workStartMin >= dayStartMin) {
+    const minutes = workStartHour * 60 + workStartMin + workTime;
+
+    const workEndHour = (minutes - (minutes % 60)) / 60;
+    const workEndMin = minutes % 60;
+
+    const dayEndHM = dayEnd.split(':');
+    const dayEndHour = Number(dayEndHM[0]);
+    const dayEndMin = Number(dayEndHM[1]);
+
+    if (workEndHour <= dayEndHour && workEndMin <= dayEndMin) {
+      return true;
+    }
+  }
+  return false;
+}
+
+isInWorkTime('08:00', '17:30', '14:00', 90); // true
+isInWorkTime('8:0', '10:0', '8:0', 120);     // true
+isInWorkTime('08:00', '14:30', '14:00', 90); // false
+isInWorkTime('14:00', '17:30', '08:0', 90);  // false
+isInWorkTime('8:00', '17:30', '08:00', 900); // false
